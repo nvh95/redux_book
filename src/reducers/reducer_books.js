c2ef62147f4a874bb42b55801cf82ae9cd427ba6
henry <henry@gotitapp.co>
2017-11-14 15:11:06 +0700

    update

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Con duong hoi giao'},
+    {title: 'Toi la mot con lua'},
+    {title: 'Toi di code dao'},
+    {title: 'Neu biet tram nam la huu han'},
+  ]
+}
